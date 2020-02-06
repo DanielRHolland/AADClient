d5968376af4eab8657fb50c816cafcbea103d5ed
dan <me@danrh.co.uk>
2020-02-06 18:27:08 +0000

    Implement autho.service

--- eksi/src/app/services/autho/autho.service.ts
+++ eksi/src/app/services/autho/autho.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConnectionSettingsService } from '../connection-settings/connection-settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthoService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private connSettings: ConnectionSettingsService) { }
+
+  public login(staffId: string, staffPassword: string) {
+    return this.httpClient.get(
+      this.getOrigin()
+      + '/l'
+      + '?id=' + staffId
+      + '&hash=' + this.hash(staffPassword));
+  }
+
+  private hash(s: string) {
+    return s;
+  }
+
+  private getOrigin() {
+    return this.connSettings.getOrigin() + '/auth';
+  }
+
 }
