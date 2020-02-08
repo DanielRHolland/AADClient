8987f3540029deb5ec32443333e8d877d439584f
dan <me@danrh.co.uk>
2020-02-08 09:03:46 +0000

    autho tracks nNumber

--- eksi/src/app/services/autho/autho.service.ts
+++ eksi/src/app/services/autho/autho.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { ConnectionSettingsService } from '../connection-settings/connection-settings.service';
+import { StaffMember } from '../../models/staff_member.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthoService {
+  private apiKey = new BehaviorSubject(null);
+  observableApiKey = this.apiKey.asObservable();
+
+  private nNumber = new BehaviorSubject(null);
+  observablenNumber = this.nNumber.asObservable();
 
   constructor(
     private httpClient: HttpClient,
     private connSettings: ConnectionSettingsService) { }
 
-  public login(staffId: string, staffPassword: string) {
-    return this.httpClient.get(
+  public login(user: StaffMember) {
+    const obs = this.httpClient.get<string>(
       this.getOrigin()
       + '/l'
-      + '?id=' + staffId
-      + '&hash=' + this.hash(staffPassword));
+      + '?id=' + user.id
+      + '&hash=' + this.hash(user.password));
+    this.changeNNumber(user.id);
+    obs.subscribe(data => this.changeApiKey( data ));
+    return obs;
   }
 
   private hash(s: string) {
@@ -25,6 +35,31 @@ export class AuthoService {
 
   private getOrigin() {
     return this.connSettings.getOrigin() + '/auth';
+  }
+
+  isStaff() {
+    return this.getApiKey() != null;
+  }
+
+  changeApiKey(apiKey: string) {
+    this.apiKey.next(apiKey);
+  }
+
+  getApiKey() {
+    return this.apiKey.getValue();
+  }
+
+  changeNNumber(nNumber: string) {
+    this.nNumber.next(nNumber);
+  }
+
+  getNNumber() {
+    return this.nNumber.getValue();
+  }
+
+  logOut() {
+    this.changeApiKey(null);
+    this.changeNNumber(null);
   }
 
 }
