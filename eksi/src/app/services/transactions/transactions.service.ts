26a0f4069a41d8f1bbee12fe6dfed9e4736c704f
dan <me@danrh.co.uk>
2020-02-11 10:36:38 +0000

    add methods for getting and posting trEntries

--- eksi/src/app/services/transactions/transactions.service.ts
+++ eksi/src/app/services/transactions/transactions.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { ConnectionSettingsService } from '../connection-settings/connection-settings.service'
 import { Transaction } from '../../models/transaction.model';
+import { TransactionEntry } from '../../models/transaction-entry.model';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,14 @@ export class TransactionsService {
   }
   public deleteTransaction(transactionId: string) {
     return this.httpClient.delete(this.getOrigin() + '/d/' + transactionId);
+  }
+
+  public getTransactionEntries(transactionId: string) {
+    return this.httpClient.get<TransactionEntry[]>(this.getOrigin() + '/entries/' + transactionId);
+  }
+
+  public saveTransactionEntries(transactionId: string, transactionEntries: TransactionEntry[]) {
+    return this.httpClient.post<TransactionEntry[]>(this.getOrigin() + '/entries/' + transactionId, transactionEntries, this.connSettings.getOptions());
   }
 
   private getOrigin() {
