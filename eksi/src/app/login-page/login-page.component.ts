7a3afde9e04b5de760b7d0b529188161cf3b5320
dan <me@danrh.co.uk>
2020-02-11 21:46:15 +0000

    login page updates isStaff on authoService

--- eksi/src/app/login-page/login-page.component.ts
+++ eksi/src/app/login-page/login-page.component.ts
@@ -13,16 +13,16 @@ import { StaffMember } from '../models/staff_member.model';
 
 export class LoginPageComponent implements OnInit {
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private authoService: AuthoService, private snackBar: MatSnackBar) { }
-  isStaff = false;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, public authoService: AuthoService, private snackBar: MatSnackBar) { }
+
   user: StaffMember = new StaffMember('', '');
   ngOnInit() {
-    this.activatedRoute.data.subscribe(x => this.isStaff = x['isStaff']);
+    this.activatedRoute.data.subscribe(x => this.authoService.isStaff = x['isStaff']);
   }
 
   onSubmit() {
     if (this.user.id && this.user.id !== '') {
-      if (this.isStaff) {
+      if (this.authoService.isStaff) {
         this.authoService.login(this.user).subscribe(data => this.success(data), error => this.failure(error));
       } else {
         this.authoService.changeNNumber(this.user.id);
